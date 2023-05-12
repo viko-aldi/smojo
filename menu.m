8abee8c2bb4e398af4fd9c996be524bebf46390e
Viko Aldi <75304610+viko-aldi@users.noreply.github.com>
2023-05-13 00:34:01 +0700

    Create menu.m

---
+++ menu.m
@@ -0,0 +1,50 @@
+\
+\ Snackbot Main Menu
+\
+
+Q: Main Menu
+
+    MM ${ "https://i.postimg.cc/8PWxp3rp/icon1-3.png" image }
+    MM **Let's start!! ${name capitalize},**
+   \ MM What do you want to ?? 
+    MM Layanan apa yang butuhkan ??
+\     MM ${ "Our Story" myButton }
+\     MM ${ "Flavors" myButton }
+\     MM ${ "Ingredients" myButton }
+\     MM ${ "Health" myButton }
+\     MM ${ "Food Pairing" myButton }
+\     MM ${ "Flavor Picker" myButton }
+\     MM ${ "Where can you buy?" myButton }
+\     MM ${ "Shipping and Delivery" myButton }
+\ UJI COBA BIKIN BUTTON
+    MM ${ "Healthy catering" myButton }
+    MM ${ "Consultation" myButton }
+    MM ${ "Self Learning" myButton }
+    M. ${ "Exit" button }
+    K: $clear faq
+    
+--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
